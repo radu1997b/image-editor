52c29ecc87cea5ea2f22a9fb3d6eb3d85e4b34c9
Radu <radu_1997b@mail.ru>
2018-02-05 20:39:30 +0200

    version 0.2

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function alegeFile(){
-  $("#photo").append("<input class=\"middle-align\" name=\"myFile\" type=\"file\">");
+  $("#photo").append("<input id=\"MyFile\" class=\"middle-align\" type=\"file\" onchange=\"puneFoto()\">");
+};
+
+function puneFoto(){
+
+  var input = document.getElementById("MyFile");
+  var fReader = new FileReader();
+  fReader.readAsDataURL(input.files[0]);
+  $("#MyFile").remove();
+  $("#photo").append(`<img id="imagine" width="300" height=400 />`);
+  fReader.onloadend = function(event){
+  var img = document.getElementById("imagine");
+  img.src = event.target.result;
+}
 };
 
 $(document).ready(alegeFile);
